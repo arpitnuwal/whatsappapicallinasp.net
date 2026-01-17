2b33dc2751d9a6d2904c2728eb8977f1aa5f544a
Arpit Nuwal <45119425+arpitnuwal@users.noreply.github.com>
2026-01-17 17:41:10 +0530

    Add files via upload

---
+++ formcode.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="formcode.aspx.cs" Inherits="formcode" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+     <div>
+            <asp:Image ID="imgQR" runat="server" Width="250" Height="250" />
+            <br />
+            <asp:Label ID="lblStatus" runat="server" Text=""></asp:Label>
+            <br /><br />
+
+            <asp:TextBox ID="txtNumber" runat="server" Placeholder="Enter number"></asp:TextBox>
+            <br /><br />
+            <asp:TextBox ID="txtMessage" runat="server" TextMode="MultiLine" Rows="5" Columns="40" Placeholder="Type message"></asp:TextBox>
+            <br /><br />
+            <asp:Button ID="btnSend" runat="server" Text="Send" OnClick="btnSend_Click" />
+            <br /><br />
+            <asp:Label ID="lblResult" runat="server" Text=""></asp:Label>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ formcode.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text;
+public partial class formcode : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadQR().Wait();
+            }}
+
+       private async Task LoadQR()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var res = await client.GetStringAsync("http://localhost:3000/qr");
+                JObject obj = JObject.Parse(res);
+
+                bool ready = (bool)obj["ready"];
+                string qr = (string)obj["qr"];
+
+                if (!ready)
+                {
+                    imgQR.ImageUrl = qr;
+                    lblStatus.Text = "Scan QR Code";
+                }
+                else
+                {
+                    lblStatus.Text = "WhatsApp Ready ✅";
+                }
+            }
+        }
+    protected void btnSend_Click(object sender, EventArgs e)
+        {
+            SendMessage().Wait();
+        }
+
+        private async Task SendMessage()
+        {
+            string number = txtNumber.Text;
+            string message = txtMessage.Text;
+
+            using (HttpClient client = new HttpClient())
+            {
+                var payload = new
+                {
+                    number = number,
+                    message = message
+                };
+
+                string json = Newtonsoft.Json.JsonConvert.SerializeObject(payload);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var res = await client.PostAsync("http://localhost:3000/send", content);
+                string result = await res.Content.ReadAsStringAsync();
+
+                lblResult.Text = result;
+            }
+        }
+}
